f8245ba92b10b4c8532110fa14db6d5b4ae59abc
Neileich <sholomkatz123@gmail.com>
2025-11-06 15:21:05 -0500

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: 'CRM' };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ fontFamily: 'sans-serif', padding: 24, maxWidth: 980, margin: '0 auto' }}>{children}</body>
-    </html>
-  );
-}
